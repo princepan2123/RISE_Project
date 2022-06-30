05811c33bcd2a6dbd934fec23a4424e727e2c53f
princepan2123 <princepan2123@gmail.com>
2022-06-30 10:47:28 +0530

    Create JwtAcessDeniedHandler.java

---
+++ projectManagement/src/main/java/com/project/projectManagement/filter/JwtAcessDeniedHandler.java
@@ -0,0 +1,32 @@
+package com.project.projectManagement.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.project.projectManagement.constant.SecurityConstant;
+import com.project.projectManagement.domain.HttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Component
+public class JwtAcessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException exception) throws IOException, ServletException {
+        HttpResponse httpResponse = new HttpResponse(HttpStatus.UNAUTHORIZED.value(),HttpStatus.UNAUTHORIZED,HttpStatus.UNAUTHORIZED.getReasonPhrase().toUpperCase(), SecurityConstant.ACcESS_DENIED_MESSAGE);
+        response.setContentType(APPLICATION_JSON_VALUE);
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        OutputStream outputStream = response.getOutputStream();
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(outputStream,httpResponse);
+        outputStream.flush();
+
+    }
+}
